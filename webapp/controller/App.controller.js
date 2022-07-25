4bd4c3dfdd588217f17fafa666feffb257094e20
Eugene Zaripov <79282844+ZaripovEM@users.noreply.github.com>
2022-07-25 15:11:51 +0300

    Step 7

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,26 @@
-sap.ui.define(['sap/ui/core/mvc/Controller'], function (Controller) {
-	'use strict'
-	return Controller.extend('sap.ui.demo.walkthrough.controller.App', {
-		onShowHello: function (oEvent) {
-			let sButtonText = oEvent.getSource().getText()
-			// show a native JavaScript alert
-			alert(`Hello World from ${sButtonText}`)
-		},
-	})
-})
+sap.ui.define(
+	[
+		'sap/ui/core/mvc/Controller',
+		'sap/m/MessageToast',
+		'sap/ui/model/json/JSONModel',
+	],
+	function (Controller, MessageToast, JSONModel) {
+		'use strict'
+		return Controller.extend('sap.ui.demo.walkthrough.controller.App', {
+			onInit: function () {
+				// set data model on view
+				var oData = {
+					recipient: {
+						name: 'Eugene',
+						surname: 'Zaripov',
+					},
+				}
+				var oModel = new JSONModel(oData)
+				this.getView().setModel(oModel)
+			},
+			onShowHello: function () {
+				MessageToast.show('Hello World')
+			},
+		})
+	}
+)
