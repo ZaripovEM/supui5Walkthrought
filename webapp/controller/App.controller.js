344c4f86db69ae8aac6e9b56a45e0b7848a69835
Eugene Zaripov <79282844+ZaripovEM@users.noreply.github.com>
2022-07-25 14:05:15 +0300

    Step 5.1

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,10 @@
 sap.ui.define(['sap/ui/core/mvc/Controller'], function (Controller) {
 	'use strict'
 	return Controller.extend('sap.ui.demo.walkthrough.controller.App', {
-		onShowHello: function () {
+		onShowHello: function (oEvent) {
+			let sButtonText = oEvent.getSource().getText()
 			// show a native JavaScript alert
-			alert('Hello World')
+			alert(`Hello World from ${sButtonText}`)
 		},
 	})
 })
